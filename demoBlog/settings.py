d0daf314d59129fe00511d7e4c7c42ef56f4e3b5
rodrigo <rodrigo.esp83@gmail.com>
2017-06-30 12:30:25 +0200

    Intento de solución del error AttributeError at /admin/ WSGIRequest' object has no attribute user

--- demoBlog/settings.py
+++ demoBlog/settings.py
@@ -40,7 +40,7 @@ INSTALLED_APPS = [
     'Post.apps.PostConfig',
 ]
 
-MIDDLEWARE = [
+MIDDLEWARE_CLASSES = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
